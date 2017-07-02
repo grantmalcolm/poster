023ff6607103b893adc6f846ad9b7330030bdd79
Grant Malcolm <grantrmalcolm@gmail.com>
2017-07-02 14:10:44 +0100

    added static pages and navigation bars

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/home' => 'pages#home'          # app home page
+  get '/userhome' => 'pages#userhome'  # users' home page
+  get '/members' => 'pages#members'    # list of members
+  get '/help' => 'pages#help'          # help pages
+  get '/about' => 'pages#about'        # project info
+
+  root 'pages#home'
 end
